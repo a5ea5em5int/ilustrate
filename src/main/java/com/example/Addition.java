e989436e30c3ff029093fbf7a8685a2f992b5582
40012435 <a.myint@napier.ac.uk>
2022-03-28 13:32:33 +0630

    pom and addition are added

---
+++ src/main/java/com/example/Addition.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Addition {
+
+    int a;
+    int b;
+    Addition(int aa, int bb)
+    {
+        a = aa;
+        b = bb;
+    }
+    int add()
+    {
+       return a+b;
+    }
+
+}
