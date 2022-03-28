4a98dfbe528cc1ce712cc7f3215f537e7cbcdf7d
40012435 <a.myint@napier.ac.uk>
2022-03-28 13:35:43 +0630

    Subtraction class is added

---
+++ src/main/java/com/example/Subtraction.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Subtraction {
+
+    int c;
+    int d;
+
+    public Subtraction(int c, int d) {
+        this.c = c;
+        this.d = d;
+    }
+    public int subtract()
+    {
+        return c-d;
+    }
+}
